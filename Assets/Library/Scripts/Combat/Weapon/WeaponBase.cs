e140118a3409ec9c98a45eccc347ca9bde03bb14
hahungphong <90634712+hahungphong@users.noreply.github.com>
2024-10-18 00:36:30 +0700

    Combat/ WeaponManager+WeaponBase set up

---
+++ Assets/Library/Scripts/Combat/Weapon/WeaponBase.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WeaponBase
+{
+    public enum WeaponType
+    {
+        Light,
+        Medium,
+        Heavy
+    }
+
+    [Header("WEAPON CONFIG")] 
+    public GameObject weaponModel;
+    [SerializeField] protected LayerMask _layerData;
+    //SerializeField] protected WeaponData _weaponData; //Make an SO with all it's atributes in it (See NOTION)
+    /// <summary>
+    /// Add anything you want the weapon to do before innitialization here
+    /// Like model, effects, Dotween and stuff like that
+    /// </summary>
+
+
+    //Implement in Weapon Base child
+    public abstract void OnInnitNormalAttack(); //Increase timer in here
+    public abstract void OnStopInnitNormalAttack(); //Stop timer
+    public abstract void OnInnitSecondaryAttack(); //
+}
+    
