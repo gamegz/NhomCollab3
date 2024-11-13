68f9a4ef851c0bb80c96908688b921d983937fc1
feveR3Dream <147809541+feveR3Dream@users.noreply.github.com>
2024-11-13 10:57:22 +0700

    Tung Fixed My Camera
    Brother... damn

--- Assets/Library/Scripts/DucAnh's Script Folder/CameraScript/CamFollowMouse.cs
+++ Assets/Library/Scripts/DucAnh's Script Folder/CameraScript/CamFollowMouse.cs
@@ -4,84 +4,50 @@ using UnityEngine;
 
 public class CamFollowMouse : MonoBehaviour
 {
-    #region Non-Serializable
-    [Header("References")]
-    private Transform bodyTransform;
-    private Vector3 velocity = Vector3.zero;
-    private Vector3 offSet;
-
-    #endregion
-
     #region Serializable
     [Header("References")]
-    [SerializeField] private GameObject player;
-    [SerializeField] private Transform camTransform;
+    [SerializeField] private Transform _player;
+    [SerializeField] private Transform _camera;
 
+    Transform target;
+    Vector2 playerV2;
+    Vector2 mouseV2;
+    Vector2 direction;
+    Vector3 _offset;
 
-
-    [Header("Values")]
-    [SerializeField] private float maxDistance = 5f;       // Maximum offset distance from the player
-    [SerializeField] private float timeToFollow = 0.1f;    // Camera follow time for smoothing
-    //[SerializeField] private float followSpeed = 10f;      // A seperate value to use [UNUSED]
+    [Header("Value")]
+    [SerializeField] private float _armMultiplier;
     #endregion
 
-    void Start()
+    private void Start()
     {
-
-        if (player == null)
-        {
-            Debug.Log("Player has not been assigned via Script or Serialized");
-        }
-
-        if (camTransform == null)
-        {
-            Debug.Log("camTransform has not been assigned via Script or Serialized");
-        }
-        else
-        {
-            offSet = camTransform.position - player.transform.position;
-        }
-
-
+        _offset = this.transform.position - _player.transform.position;    
     }
 
-    void LateUpdate() 
+    void Update()
     {
-        if (camTransform != null)
-        {
-
-            bodyTransform = player.transform.Find("Body").GetComponent<Transform>();
-            
-            camTransform.position = bodyTransform.position + offSet;
-            
-            camTransform.rotation = Quaternion.identity;
-
-            CameraWork();
-        }
+        this.transform.position = _player.position + _offset;
+        CameraWork();
     }
 
 
     void CameraWork()
     {
         // 1. Get the player position in screen space and mouse position
-        Vector2 playerScreenPos = Camera.main.WorldToScreenPoint(bodyTransform.position);
-        Vector2 mouseScreenPos = Input.mousePosition;
+        playerV2 = Camera.main.WorldToScreenPoint(_player.position);
+        mouseV2 = Input.mousePosition;
 
         // 2. Calculate the difference between player position and mouse position
-        Vector2 difference = mouseScreenPos - playerScreenPos;
+        direction = (RemapToScreenCenter(mouseV2) - RemapToScreenCenter(playerV2));
 
-        // 3. Clamp the difference to limit how far the camera can be offset by the mouse
-        if (difference.magnitude > maxDistance)
-        {
-            difference = difference.normalized * maxDistance;
-        }
+        _camera.transform.localPosition = new Vector3(direction.x * _armMultiplier, 0, direction.y * _armMultiplier);
+    }
 
-        // 4. Calculate the target position for the camera in world space
-        Vector3 targetWorldPos = camTransform.position + (new Vector3(difference.x, 0, difference.y) * 1/2f);
+    public Vector2 RemapToScreenCenter(Vector2 pixelPosition)
+    {
+        float xNormalized = Mathf.Clamp((pixelPosition.x - Screen.width / 2) / (Screen.width / 2), -1, 1);
+        float yNormalized = Mathf.Clamp((pixelPosition.y - Screen.height / 2) / (Screen.height / 2), -1, 1);
 
-
-        // 5. Smoothly move the camera towards the target position
-        transform.position = Vector3.SmoothDamp(transform.position, targetWorldPos, ref velocity, timeToFollow);
-        //transform.position = targetWorldPos;
+        return new Vector2(xNormalized, yNormalized);
     }
 }
